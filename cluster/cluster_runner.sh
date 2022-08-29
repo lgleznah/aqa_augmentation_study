a59185159bba7c6553f78ec5b3e34d8de6f400e6
lgleznah <luisglez02@gmail.com>
2022-08-29 10:53:50 +0000

    Changed cluster runner to not mount old AVA dir

--- cluster/cluster_runner.sh
+++ cluster/cluster_runner.sh
@@ -1,4 +1,4 @@
 EXPERIMENT_ID="${PBS_ARRAY_INDEX:-0}"
 cd $AQA_AUGMENT_ROOT
 
-/usr/local/bin/singularity exec --nv -B /home/Shared-AVA/AVA-dataset:/home/Shared-AVA/AVA-dataset:ro ~/docker_images/tensorflow.sif python $file $EXPERIMENT_ID $experiments
+/usr/local/bin/singularity exec --nv ~/docker_images/tensorflow.sif python $file $EXPERIMENT_ID $experiments
