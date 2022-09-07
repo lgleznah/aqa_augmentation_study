63d25c52ba6608305cb5b4bfad7fb2e1528dd481
lgleznah <luisglez02@gmail.com>
2022-09-07 15:57:15 +0000

    Added launch instructions to benchmark runner

--- cluster/benchmark_runner.sh
+++ cluster/benchmark_runner.sh
@@ -1,3 +1,5 @@
+# qsub -l select=ngpus=1 -v results=results_NAS.txt,parallel=false,path=/home/lgleznah/src/AVA-dataset/,AQA_AUGMENT_ROOT benchmark_runner.sh
+
 cd $AQA_AUGMENT_ROOT
 
 /usr/local/bin/singularity exec ~/docker_images/tensorflow.sif python cluster/benchmark.py $results $parallel $path
