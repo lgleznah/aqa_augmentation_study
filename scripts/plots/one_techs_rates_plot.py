750d4c5d6fb3e5915405631e678251fc916270ca
lgleznah <luisglez02@gmail.com>
2022-10-20 12:21:25 +0200

    Added plotting script for one-tech experiments

--- scripts/plots/one_techs_rates_plot.py
+++ scripts/plots/one_techs_rates_plot.py
@@ -1,25 +1,27 @@
-from cProfile import label
 import matplotlib.pyplot as plt
 import os
+import sys
 
 def main():
     experiment_names = ['brightness', 'contrast', 'flip', 'rotation', 'translation', 'zoom']
-    rates = [10, 25, 50, 100]
+    metric_lines = {'balanced_accuracy': 4, 'accuracy': 5, 'Mean_EMD': 6, 'MSE': 7}
+
+    experiment_group = sys.argv[1]
+    metric_name = sys.argv[2]
+    metric_line = metric_lines[metric_name]
+    rates = [ int(x) for x in sys.argv[3:] ]
 
     for exp in experiment_names:
-        accuracies = []
-        bal_accuracies = []
+        metric_values = []
         for rate in rates:
-            with open(os.path.join('augmentation-results', 'one_techs_rates', f'{exp}_{rate}_results.txt'), 'r') as f:
+            with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{rate}_results.txt'), 'r') as f:
                 exp_results = f.readlines()
-                accuracies.append(float(exp_results[5].split(':')[1][1:]))
-                bal_accuracies.append(float(exp_results[4].split(':')[1][1:]))
+                metric_values.append(float(exp_results[metric_line].split(':')[1][1:]))
         
-        plt.plot(rates, accuracies, label=exp)
+        plt.plot(rates, metric_values, label=exp)
     
     plt.legend()
-    plt.savefig('plot_accuracy.png')
-
+    plt.savefig(os.path.join('figures', experiment_group, f'plot_{metric_name}.png'))
 
 if __name__ == "__main__":
     main()
