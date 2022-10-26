6ab0ff3b8db0c959271f168376747125a6a6ed3d
lgleznah <luisglez02@gmail.com>
2022-10-26 13:32:28 +0200

    Adapted sequential launcher script to only use experiment names

--- scripts/experiments/sequential_launcher.sh
+++ scripts/experiments/sequential_launcher.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-EXPERIMENT_FILE="$AQA_AUGMENT_EXPERIMENTS_PATH/$2"
+EXPERIMENT_NAME=$2
+EXPERIMENT_FILE="$AQA_AUGMENT_EXPERIMENTS_PATH/$2.yaml"
 # Count the number of lines from the YAML file
 JOBS=$(grep 'name' $EXPERIMENT_FILE | wc -l)
 JOBS_ITER=$(($JOBS-1))
@@ -14,5 +15,5 @@ FILE=$AQA_AUGMENT_ROOT/$1
 for i in $(seq 0 $JOBS_ITER)
 do
     echo "Running experiment with index ${i}"
-    python $FILE $i $EXPERIMENT_FILE $RERUN
+    python $FILE $i $EXPERIMENT_NAME $RERUN
 done
