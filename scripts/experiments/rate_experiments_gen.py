de260c9d39370490708abdddec39d1e696dd6f68
lgleznah <luisglez02@gmail.com>
2022-11-23 18:44:43 +0100

    Changed rate experiments generator to take intensity into account

--- scripts/experiments/rate_experiments_gen.py
+++ scripts/experiments/rate_experiments_gen.py
@@ -2,83 +2,94 @@ import sys
 
 def main():
     filename = sys.argv[1]
-    rates = [float(rate) for rate in sys.argv[2:]]
+    intensity = sys.argv[2]
+    rates = [float(rate) for rate in sys.argv[3:]]
 
     brightness_template = (
-        '  - name: brightness_{}\n'
+        '  - name: brightness_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: brightness\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
         '        args:\n'
-        '          - 0.5: float\n'
+        '          - {intensity}: float\n'
         '        kwargs:\n'
         '          value_range:\n' 
         '            (-1,1): tuple\n'
     )
 
     contrast_template = (
-        '  - name: contrast_{}\n'
+        '  - name: contrast_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: contrast\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
         '        args:\n'
-        '          - 0.5: float\n'
+        '          - {intensity}: float\n'
     )
 
     flip_template = (
-        '  - name: flip_{}\n'
+        '  - name: flip_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: flip\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
+        '        args:\n'
+        '          - horizontal: string\n'
     )
 
     rotation_template = (
-        '  - name: rotation_{}\n'
+        '  - name: rotation_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: rotation\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
         '        args:\n'
-        '          - 1.0: float\n'
+        '          - {intensity}: float\n'
+        '        kwargs:\n'
+        '          fill_mode:\n'
+        '            constant: string\n'
     )
 
     translation_template = (
-        '  - name: translation_{}\n'
+        '  - name: translation_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: translation\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
         '        args:\n'
-        '          - 0.5: float\n'
-        '          - 0.5: float\n'
+        '          - {intensity}: float\n'
+        '          - {intensity}: float\n'
+        '        kwargs:\n'
+        '          fill_mode:\n'
+        '            constant: string\n'
     )
 
     zoom_template = (
-        '  - name: zoom_{}\n'
+        '  - name: zoom_{rate_pct}\n'
         '    base_model: mobilenet\n'
         '    output_format: distribution\n'
         '    batch_size: 128\n'
         '    layers:\n'
         '      - layer: zoom\n'
-        '        rate: {}\n'
+        '        rate: {rate}\n'
         '        args:\n'
-        '          - 0.5: float\n'
+        '          - {intensity}: float\n'
         '        kwargs:\n'
         '          width_factor:\n' 
-        '            0.5: float\n'
+        '            {intensity}: float\n'
+        '          fill_mode:\n'
+        '            constant: string\n'
     )
 
     templates = [brightness_template, contrast_template, flip_template, rotation_template, translation_template, zoom_template]
@@ -86,7 +97,7 @@ def main():
     with open(filename, 'w') as f:
         for template in templates:
             for rate in rates:
-                print(template.format(int(rate*100), rate), file=f)
+                print(template.format(rate_pct = int(rate*100), rate = rate, intensity = intensity), file=f)
 
 
 if __name__ == '__main__':
