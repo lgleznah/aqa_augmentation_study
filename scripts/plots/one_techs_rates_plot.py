112f458aaf75b323269114d5506eaf2b38ce8f71
lgleznah <luisglez02@gmail.com>
2022-12-22 19:15:42 +0100

    Changed plotting script to plot all metrics at once

--- scripts/plots/one_techs_rates_plot.py
+++ scripts/plots/one_techs_rates_plot.py
@@ -19,10 +19,14 @@ def main():
 
     #experiment_names = ['brightness', 'contrast', 'flip', 'rotation', 'translation', 'zoom']
     experiment_names = ['brightness', 'flip', 'rotation', 'translation', 'zoom']
+    metrics_per_type = {
+        'distribution': ['binary_balanced_accuracy', 'binary_accuracy', 'bal_accuracy_maxrating', 'accuracy_maxrating', 'bal_accuracy_meanbin', 'accuracy_meanbin', 'mean_emd', 'mean_squared_error', 'average_prediction_entropy', 'average_groundtruth_entropy'],
+        'tenclass': ['binary_balanced_accuracy', 'balanced_accuracy', 'mean_squared_error']
+    }
 
     # Parse command-line arguments
     experiment_group = sys.argv[1]
-    metric_name = sys.argv[2]
+    metrics = metrics_per_type[sys.argv[2]]
     baseline_name = sys.argv[3]
     rates = [ int(x) for x in sys.argv[4:] ]
 
@@ -31,39 +35,42 @@ def main():
     if not os.path.exists(plot_dir):
         os.mkdir(plot_dir)
 
-    # Fetch baseline result, and plot it as a horizontal line
-    with open(os.path.join('augmentation-results', baseline_name, 'baseline_results.json'), 'r') as f:
-        baseline_results = json.load(f)
-        baseline_metric =baseline_results[metric_name]
+    # TODO Add different limits per metric
+    for metric_name in metrics:
+        # Fetch baseline result, and plot it as a horizontal line
+        with open(os.path.join('augmentation-results', baseline_name, 'baseline_results.json'), 'r') as f:
+            baseline_results = json.load(f)
+            baseline_metric = baseline_results[metric_name]
 
-    # Plot results for all augmentation techniques and given rates
-    for exp in experiment_names:
-        metric_values = [baseline_metric]
-        for rate in rates:
-            with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{rate}_results.json'), 'r') as f:
-                exp_results = json.load(f)
-                metric_values.append(exp_results[metric_name])
-        
-        plt.plot([0] + rates, metric_values, label=exp.capitalize())
+        # Plot results for all augmentation techniques and given rates
+        for exp in experiment_names:
+            metric_values = [baseline_metric]
+            for rate in rates:
+                with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{rate}_results.json'), 'r') as f:
+                    exp_results = json.load(f)
+                    metric_values.append(exp_results[metric_name])
+            
+            plt.plot([0] + rates, metric_values, label=exp.capitalize())
 
-    plt.axhline(y = baseline_metric, color = 'g', linestyle = '--', label = 'Baseline')
+        plt.axhline(y = baseline_metric, color = 'g', linestyle = '--', label = 'Baseline')
 
-    # Set vertical limits, if plotting accuracy or balanced accuracy. Leave no upper limit, otherwise
-    if metric_name in ['balanced_accuracy', 'accuracy']:
+        # Set vertical limits, if plotting accuracy or balanced accuracy. Leave no upper limit, otherwise
+        if metric_name in ['balanced_accuracy', 'accuracy']:
+            plt.ylim(0,1)
+        else:
+            plt.ylim(bottom = 0)
+
+        # If plotting balanced accuracy, also plot a THICC red line on 0.5, to highlight ZeroR performance
+        if metric_name == 'balanced_accuracy':
+            plt.axhline(y = 0.5, color = 'r', linestyle = 'dotted', label = 'ZeroR performance', linewidth = 1.5)
+
+        plt.xticks([0,10,20,30,40,50,60,70,80,90,100])
+        plt.xlim(0, 100)
+
         plt.ylim(0,1)
-    else:
-        plt.ylim(bottom = 0)
-
-    # If plotting balanced accuracy, also plot a THICC red line on 0.5, to highlight ZeroR performance
-    if metric_name == 'balanced_accuracy':
-        plt.axhline(y = 0.5, color = 'r', linestyle = 'dotted', label = 'ZeroR performance', linewidth = 1.5)
-
-    plt.xticks([0,10,20,30,40,50,60,70,80,90,100])
-    plt.xlim(0, 100)
-
-    plt.ylim(0,1)
-    plt.legend()
-    plt.savefig(os.path.join(plot_dir, f'plot_{metric_name}.png'))
+        plt.legend()
+        plt.savefig(os.path.join(plot_dir, f'plot_{metric_name}.png'))
+        plt.close()
 
 if __name__ == "__main__":
     main()
