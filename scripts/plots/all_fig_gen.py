3a56bd1ade111415140fff008eecd0fa3bfa7636
lgleznah <luisglez02@gmail.com>
2023-04-26 11:19:45 +0200

    Added intensities experiments

--- scripts/plots/all_fig_gen.py
+++ scripts/plots/all_fig_gen.py
@@ -1,7 +1,8 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib
-from one_techs_rates_plot import fig_gen
+from one_techs_rates_plot import fig_gen as rates_fig_gen
+from one_techs_intensities_plot import fig_gen as intensities_fig_gen
 
 baselines = [
     'baseline_ava_small',
@@ -45,6 +46,34 @@ high_intensity = [
     'one_techs_high_intensity_photozilla_ovr_wildlife',
 ]
 
+low_probability = [
+    'one_techs_low_proba_ava_small',
+    'one_techs_low_proba_photozilla_ovr_aerial',
+    'one_techs_low_proba_photozilla_ovr_architecture',
+    'one_techs_low_proba_photozilla_ovr_event',
+    'one_techs_low_proba_photozilla_ovr_fashion',
+    'one_techs_low_proba_photozilla_ovr_food',
+    'one_techs_low_proba_photozilla_ovr_nature',
+    'one_techs_low_proba_photozilla_ovr_sports',
+    'one_techs_low_proba_photozilla_ovr_street',
+    'one_techs_low_proba_photozilla_ovr_wedding',
+    'one_techs_low_proba_photozilla_ovr_wildlife',
+]
+
+high_probability = [
+    'one_techs_high_proba_ava_small',
+    'one_techs_high_proba_photozilla_ovr_aerial',
+    'one_techs_high_proba_photozilla_ovr_architecture',
+    'one_techs_high_proba_photozilla_ovr_event',
+    'one_techs_high_proba_photozilla_ovr_fashion',
+    'one_techs_high_proba_photozilla_ovr_food',
+    'one_techs_high_proba_photozilla_ovr_nature',
+    'one_techs_high_proba_photozilla_ovr_sports',
+    'one_techs_high_proba_photozilla_ovr_street',
+    'one_techs_high_proba_photozilla_ovr_wedding',
+    'one_techs_high_proba_photozilla_ovr_wildlife',
+]
+
 rates = [25, 50, 75, 100]
 
 scale = 3
@@ -56,28 +85,81 @@ def main():
 
     for experiments, plot_name in [(low_intensity, 'low_intensity'), (high_intensity, 'high_intensity')]:
         fig = plt.figure(figsize=(8.27*scale, 11.69*scale))
+        w, h = 0.5 - margin_frac * 2, 0.15 - margin_frac
         axes = {
-            'ax0': fig.add_axes([0.25 + margin_frac, 0.85 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax1': fig.add_axes([0 + margin_frac, 0.7 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax2': fig.add_axes([0.5 + margin_frac, 0.7 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax3': fig.add_axes([0 + margin_frac, 0.55 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax4': fig.add_axes([0.5 + margin_frac, 0.55 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax5': fig.add_axes([0 + margin_frac, 0.4 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax6': fig.add_axes([0.5 + margin_frac, 0.4 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax7': fig.add_axes([0 + margin_frac, 0.25 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax8': fig.add_axes([0.5 + margin_frac, 0.25 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax9': fig.add_axes([0 + margin_frac, 0.1 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac]),
-            'ax10': fig.add_axes([0.5 + margin_frac, 0.1 - margin_frac, 0.5 - margin_frac * 2, 0.15 - margin_frac])
+            'ax0': fig.add_axes([0.25 + margin_frac, 0.85 - margin_frac, w, h]),
+            'ax1': fig.add_axes([0 + margin_frac, 0.7 - margin_frac, w, h]),
+            'ax2': fig.add_axes([0.5 + margin_frac, 0.7 - margin_frac, w, h]),
+            'ax3': fig.add_axes([0 + margin_frac, 0.55 - margin_frac, w, h]),
+            'ax4': fig.add_axes([0.5 + margin_frac, 0.55 - margin_frac, w, h]),
+            'ax5': fig.add_axes([0 + margin_frac, 0.4 - margin_frac, w, h]),
+            'ax6': fig.add_axes([0.5 + margin_frac, 0.4 - margin_frac, w, h]),
+            'ax7': fig.add_axes([0 + margin_frac, 0.25 - margin_frac, w, h]),
+            'ax8': fig.add_axes([0.5 + margin_frac, 0.25 - margin_frac, w, h]),
+            'ax9': fig.add_axes([0 + margin_frac, 0.1 - margin_frac, w, h]),
+            'ax10': fig.add_axes([0.5 + margin_frac, 0.1 - margin_frac, w, h])
         }
 
         # Plot AVA small part
-        fig_gen(experiments[0], 'binary', baselines[0], rates, ax=axes['ax0'], metric='binary_balanced_accuracy', legend=False)
+        rates_fig_gen(experiments[0], 'binary', baselines[0], rates, ax=axes['ax0'], metric='binary_balanced_accuracy', legend=False)
 
         # Plot Photozilla experiments
         for exp_number, experiment in enumerate(experiments[1:], 1):
-            fig_gen(experiment, 'binary', baselines[exp_number], rates, ax=axes[f'ax{exp_number}'], metric='binary_balanced_accuracy', legend=False)
+            rates_fig_gen(experiment, 'binary', baselines[exp_number], rates, ax=axes[f'ax{exp_number}'], metric='binary_balanced_accuracy', legend=False)
 
         fig.legend(axes['ax0'].lines, ['Brightness', 'Contrast', 'Flip', 'Rotation', 'Translation', 'Zoom', 'Baseline'])
+        plt.savefig(os.path.join('figures', f'{plot_name}.eps'))
+        plt.close()
+
+    for experiments, plot_name in [(low_probability, 'low_probability'), (high_probability, 'high_probability')]:
+        fig = plt.figure(figsize=(8.27*scale, 11.69*scale))
+        w, h = (0.5 - margin_frac * 2) / 2, 0.15 - margin_frac
+        axes = {
+            'ax0': (
+                fig.add_axes([0.25 + margin_frac, 0.85 - margin_frac, w, h]),
+                fig.add_axes([0.25 + margin_frac * 1.5 + w, 0.85 - margin_frac, w, h])),
+            'ax1': (
+                fig.add_axes([0 + margin_frac, 0.7 - margin_frac, w, h]),
+                fig.add_axes([0 + margin_frac * 1.5 + w, 0.7 - margin_frac, w, h])),
+            'ax2': (
+                fig.add_axes([0.5 + margin_frac, 0.7 - margin_frac, w, h]),
+                fig.add_axes([0.5 + margin_frac * 1.5 + w, 0.7 - margin_frac, w, h])),
+            'ax3': (
+                fig.add_axes([0 + margin_frac, 0.55 - margin_frac, w, h]),
+                fig.add_axes([0 + margin_frac * 1.5 + w, 0.55 - margin_frac, w, h])),
+            'ax4': (
+                fig.add_axes([0.5 + margin_frac, 0.55 - margin_frac, w, h]),
+                fig.add_axes([0.5 + margin_frac * 1.5 + w, 0.55 - margin_frac, w, h])),
+            'ax5': (
+                fig.add_axes([0 + margin_frac, 0.4 - margin_frac, w, h]),
+                fig.add_axes([0 + margin_frac * 1.5 + w, 0.4 - margin_frac, w, h])),
+            'ax6': (
+                fig.add_axes([0.5 + margin_frac, 0.4 - margin_frac, w, h]),
+                fig.add_axes([0.5 + margin_frac * 1.5 + w, 0.4 - margin_frac, w, h])),
+            'ax7': (
+                fig.add_axes([0 + margin_frac, 0.25 - margin_frac, w, h]),
+                fig.add_axes([0 + margin_frac * 1.5 + w, 0.25 - margin_frac, w, h])),
+            'ax8': (
+                fig.add_axes([0.5 + margin_frac, 0.25 - margin_frac, w, h]),
+                fig.add_axes([0.5 + margin_frac * 1.5 + w, 0.25 - margin_frac, w, h])),
+            'ax9': (
+                fig.add_axes([0 + margin_frac, 0.1 - margin_frac, w, h]),
+                fig.add_axes([0 + margin_frac * 1.5 + w, 0.1 - margin_frac, w, h])),
+            'ax10': (
+                fig.add_axes([0.5 + margin_frac, 0.1 - margin_frac, w, h]),
+                fig.add_axes([0.5 + margin_frac * 1.5 + w, 0.1 - margin_frac, w, h]))
+        }
+
+        # Plot AVA small part
+        intensities_fig_gen(experiments[0], 'binary', baselines[0], rates, ax1=axes['ax0'][0], ax2=axes['ax0'][1], metric='binary_balanced_accuracy', legend=False)
+        axes['ax0'][1].set_yticks([])
+
+        # Plot Photozilla experiments
+        for exp_number, experiment in enumerate(experiments[1:], 1):
+            intensities_fig_gen(experiment, 'binary', baselines[exp_number], rates, ax1=axes[f'ax{exp_number}'][0], ax2=axes[f'ax{exp_number}'][1], metric='binary_balanced_accuracy', legend=False)
+            axes[f'ax{exp_number}'][1].set_yticks([])
+
+        fig.legend(axes['ax0'][0].lines, ['Brightness', 'Contrast', 'Rotation', 'Translation', 'Zoom', 'Baseline'])
         plt.savefig(os.path.join('figures', f'{plot_name}.eps'))
         plt.close()
 
--- scripts/plots/one_techs_intensities_plot.py
+++ scripts/plots/one_techs_intensities_plot.py
@@ -3,84 +3,136 @@ import os
 import json
 import sys
 
-def main():
-    '''
-    Plot the results of experiments in which the augmentation intensity has been modified.
-    Since flipping experiments do not have an intensity per-sé, these are plotted
-    as a barplot, for all possible kinds of flipping
+experiment_names_colors = {
+    'brightness': '#0061ff', 
+    'contrast': '#60efff', 
+    'flip': '#f7b267', 
+    'rotation': '#f79d65', 
+    'translation': '#f4845f', 
+    'zoom': '#f27059'
+}
 
-    Command-line arguments:
-        - 1st argument: the name of the experiment group to plot
-        - 2nd argument: the name of the metric to plot
-        - 3rd argument: the name of the corresponding, unaugmented baseline
-        - 4th argument and so on: the specific rates to plot
+metrics_per_type = {
+    'distribution': ['binary_balanced_accuracy', 'binary_accuracy', 'bal_accuracy_maxrating', 'accuracy_maxrating', 'bal_accuracy_meanbin', 'accuracy_meanbin', 'mean_emd', 'mean_squared_error', 'average_prediction_entropy', 'average_groundtruth_entropy'],
+    'tenclass': ['binary_balanced_accuracy', 'binary_accuracy', 'mean_squared_error'],
+    'binary': ['binary_balanced_accuracy', 'binary_accuracy', 'mean_squared_error']
+}
+
+titles_per_baseline = {
+    'baseline_ava_small': 'AVA small',
+    'baseline_photozilla_ovr_aerial': 'Photozilla-aerial',
+    'baseline_photozilla_ovr_architecture': 'Photozilla-architecture',
+    'baseline_photozilla_ovr_event': 'Photozilla-event',
+    'baseline_photozilla_ovr_fashion': 'Photozilla-fashion',
+    'baseline_photozilla_ovr_food': 'Photozilla-food',
+    'baseline_photozilla_ovr_nature': 'Photozilla-nature',
+    'baseline_photozilla_ovr_sports': 'Photozilla-sports',
+    'baseline_photozilla_ovr_street': 'Photozilla-street',
+    'baseline_photozilla_ovr_wedding': 'Photozilla-wedding',
+    'baseline_photozilla_ovr_wildlife': 'Photozilla-wildlife',
+}
+
+flip_values = ['h', 'v', 'hv']
+
+def fig_gen(experiment_group, out_format, baseline_name, rates, save=False, metric=None, ax1=None, ax2=None, legend=True):
+    '''
+    Plot the results of experiments in which the augmentation probability has been modified.
+
+    Arguments:
+        - experiment_group: the name of the experiment group to plot
+        - out_format: the output format of the experiment group
+        - baseline_name: the name of the corresponding, unaugmented baseline
+        - rates: a list with the specific rates to plot
+        - save: whether to save the figure (True) or to return it (False)
+        - metric: what metric to print. If None, generate plots of all metrics
+        - ax1 and ax2: Matplotlib Axes in which to print. Can be "None" if "save" is True
 
     Returns:
-        - Nothing. The plot itself is saved on figures/{experiment_group}/plot_{metric_name}
+        - Nothing or the figure, depending on the value of "save"
     '''
-
-    experiment_names = ['brightness', 'contrast', 'flip', 'rotation', 'translation', 'zoom']
-    flip_values = ['horizontal', 'vertical', 'both']
-
-    # Parse command-line arguments
-    experiment_group = sys.argv[1]
-    metric_name = sys.argv[2]
-    baseline_name = sys.argv[3]
-    rates = [ int(x) for x in sys.argv[4:] ]
+    metrics = metrics_per_type[out_format]
 
     # Create directory in which to save plot (if it does not exist)
     plot_dir = os.path.join('figures', experiment_group)
     if not os.path.exists(plot_dir):
         os.mkdir(plot_dir)
 
-    # Plot results for all augmentation techniques and given intensities
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,8), sharey='row')
-    for exp in experiment_names:
-        metric_values = []
-        if (exp != 'flip'):
-            for rate in rates:
-                with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{rate}_results.json'), 'r') as f:
-                    exp_results = json.load(f)
-                    metric_values.append(exp_results[metric_name])
-            
-            ax1.set_xlabel("Augmentation intensity")
-            ax1.set_ylabel(metric_name.replace("_", " ").capitalize())
-            ax1.plot(rates, metric_values, label=exp)
+    metrics_to_plot = metrics if metric is None else [metric]
 
+    # TODO Add different limits per metric
+    for metric_name in metrics_to_plot:
+        axes_to_plot_1 = plt.subplots(1,1)[1] if ax1 is None else ax1
+        axes_to_plot_2 = plt.subplots(1,1)[1] if ax2 is None else ax2
+        # Fetch baseline result, and plot it as a horizontal line
+        with open(os.path.join('augmentation-results', baseline_name, 'baseline_results.json'), 'r') as f:
+            baseline_results = json.load(f)
+            baseline_metric = baseline_results[metric_name]
+
+        # Plot results for all augmentation techniques and given rates
+        for exp in experiment_names_colors:
+            metric_values = [baseline_metric]
+            if (exp != 'flip'):
+                for rate in rates:
+                    with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{rate}_results.json'), 'r') as f:
+                        exp_results = json.load(f)
+                        metric_values.append(exp_results[metric_name])
+                
+                axes_to_plot_1.set_ylabel(metric_name.replace("_", " ").capitalize())
+                axes_to_plot_1.plot([0] + rates, metric_values, label=exp, c=experiment_names_colors[exp])
+
+            else:
+                for flip in flip_values:
+                    with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{flip}_results.json'), 'r') as f:
+                        exp_results = json.load(f)
+                        metric_values.append(exp_results[metric_name])
+
+                axes_to_plot_2.yaxis.set_tick_params(labelbottom=True)
+                axes_to_plot_2.bar(flip_values, metric_values[1:], color=experiment_names_colors[exp])
+
+        axes_to_plot_1.axhline(y = baseline_metric, color = 'g', linestyle = (0,(1,5)), label = 'Baseline')
+        axes_to_plot_2.axhline(y = baseline_metric, color = 'g', linestyle = (0,(1,5)), label = 'Baseline')
+
+        # Set vertical limits, if plotting accuracy or balanced accuracy. Leave no upper limit, otherwise
+        if metric_name in ['balanced_accuracy', 'accuracy']:
+            axes_to_plot_1.set_ylim(0,1)
+            axes_to_plot_2.set_ylim(0,1)
         else:
-            for flip in flip_values:
-                with open(os.path.join('augmentation-results', experiment_group, f'{exp}_{flip}_results.json'), 'r') as f:
-                    exp_results = json.load(f)
-                    metric_values.append(exp_results[metric_name])
+            axes_to_plot_1.set_ylim(bottom = 0)
+            axes_to_plot_2.set_ylim(bottom = 0)
 
-            ax2.set_xlabel("Flip type")
-            ax2.yaxis.set_tick_params(labelbottom=True)
-            ax2.bar(flip_values, metric_values)
+        # If plotting balanced accuracy, also plot a THICC red line on 0.5, to highlight ZeroR performance
+        if metric_name == 'balanced_accuracy':
+            axes_to_plot_1.axhline(y = 0.5, color = 'r', linestyle = 'dotted', label = 'ZeroR performance', linewidth = 1.5)
+            axes_to_plot_2.axhline(y = 0.5, color = 'r', linestyle = 'dotted', label = 'ZeroR performance', linewidth = 1.5)
 
-    # Fetch baseline result, and plot it as a horizontal line, on both figures
-    with open(os.path.join('augmentation-results', baseline_name, 'baseline_results.json'), 'r') as f:
-        baseline_results = json.load(f)
-        baseline_metric = baseline_results[metric_name]
+        axes_to_plot_1.set_xticks([0,25,50,75,100])
+        axes_to_plot_2.set_xticks([0,1,2])
+        axes_to_plot_2.set_xticklabels(['horizontal', 'vertical', 'both'])
+        axes_to_plot_1.set_xlim(0, 100)
+        axes_to_plot_2.set_xlim(-0.5, 2.5)
 
-    ax1.axhline(y = baseline_metric, color = 'g', linestyle = '--', label = 'Baseline')
-    ax2.axhline(y = baseline_metric, color = 'g', linestyle = '--', label = 'Baseline')
+        axes_to_plot_1.set_ylim(0,1)
+        axes_to_plot_2.set_ylim(0,1)
 
-    # Set vertical limits, if plotting accuracy or balanced accuracy. Leave no upper limit, otherwise
-    if metric_name in ['balanced_accuracy', 'accuracy']:
-        plt.ylim(0,1)
-    else:
-        plt.ylim(bottom = 0)
+        if legend:
+            axes_to_plot_1.legend()
+            axes_to_plot_2.legend()
 
-    # If plotting balanced accuracy, also plot a THICC red line on 0.5, to highlight ZeroR performance
-    if metric_name == 'balanced_accuracy':
-        ax1.axhline(y = 0.5, color = 'r', linestyle = 'dotted', label = 'ZeroR performance', linewidth = 1.5)
+        axes_to_plot_1.set_xlabel('Augmentation intensity')
+        axes_to_plot_2.set_xlabel('Flipping technique')
+        axes_to_plot_1.set_ylabel(metric_name.replace('_', ' ').capitalize())
+        axes_to_plot_1.set_title(titles_per_baseline[baseline_name], position=(1, 1))
 
-    ax1.set_xticks([10,20,30,40,50,60,70,80,90,100])
-    ax1.set_xlim(10, 100)
-
-    ax1.legend()
-    ax2.legend()
-    plt.savefig(os.path.join(plot_dir, f'plot_{metric_name}.png'))
+        if save:
+            plt.savefig(os.path.join(plot_dir, f'plot_{metric_name}.eps'))
+        
 
 if __name__ == "__main__":
-    main()
+
+    # Parse command-line arguments
+    experiment_group = sys.argv[1]
+    out_format = sys.argv[2]
+    baseline_name = sys.argv[3]
+    rates = [ int(x) for x in sys.argv[4:] ]
+
+    fig_gen(experiment_group, out_format, baseline_name, rates, save=True)
